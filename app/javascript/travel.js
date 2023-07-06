f737690564b8943c3d5bc0223d107e380aaf6b84
Tanaka1123 <tamatech11@gmail.com>
2023-07-06 11:14:54 +0900

    topページ作成

---
+++ app/javascript/travel.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu");
+//ヘッダーがulタグの位置に来たら、ヘッダーを固定する。
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= menu.offsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
