4c810e63b15874901f6994825e5ff148a0e729b3
Tanaka1123 <tamatech11@gmail.com>
2023-07-19 15:14:17 +0900

    bookmarkのテスト未完了

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'bookmarkの保存' do
+    it 'すべての値が正しく入力されていれば保存できること' do
+      user = FactoryBot.create(:user)
+      bookmark = FactoryBot.build(:bookmark, user: user)
+      expect(bookmark).to be_valid
+    end
+  end
 end
