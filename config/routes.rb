290526f12e03a43d63d61e1608ffe15888d4c604
Tanaka1123 <tamatech11@gmail.com>
2023-07-27 17:39:27 +0900

    chatGPT導入

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: "travels#index"
   resources :bookmarks
   get '/roulette', to: 'roulette#index'
-
   get "/p5.min.js", to: redirect("/assets/p5.min.js")
 get "/app.js", to: redirect("/assets/app.js")
+get 'chatbots', to: 'chatbots#index'
+post 'chatbots', to: 'chatbots#create'
 end
